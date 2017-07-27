f209b85403f15a79a049b6789987988d15b477ee
Kevin Kawai <kevin.fredricks@gmail.com>
2017-07-27 01:55:54 +0000

    Added Stripe and the ability to make payments on premium courses, free courses are free

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,4 +1,6 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_authorized_for_current_lesson
 
   def show
   end
@@ -10,4 +12,10 @@ class LessonsController < ApplicationController
     @current_lesson ||= Lesson.find(params[:id])
   end
 
+
+  def require_authorized_for_current_lesson
+    if current_user.enrolled_in?(current_lesson.section.course) == false
+     redirect_to course_path(current_lesson.section.course), alert: "You need to enroll first"
+    end
+  end
 end
